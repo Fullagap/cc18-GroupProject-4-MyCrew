a5db5479ea971d7419ef7c6aa7c52a3d1f6442f6
Natee-B <nateebunyalug1994@gmail.com>
2024-10-31 17:02:53 +0700

    CSSAdminHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import AppRoute from './routes/AppRoute'
+import AppRoute from './routes/AppRoute'
 import { ToastContainer,toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
-     {/* <AppRoute/> */}
-     {/* <ToastContainer
+     <AppRoute/>
+     <ToastContainer
       position="top-right"
       autoClose={2000}
       hideProgressBar={false}
@@ -18,7 +18,7 @@ function App() {
       draggable
       pauseOnHover
       theme="dark"
-      /> */}
+      />
 
     </div>
   )
--- src/pages/admin/home-admin.jsx
+++ src/pages/admin/home-admin.jsx
@@ -6,55 +6,45 @@ import { VscGitPullRequestGoToChanges } from "react-icons/vsc";
 import { TbReportMoney } from "react-icons/tb";
 
 export default function HomeAdmin() {
+  const classLink =
+    "flex flex-col border w-32 rounded-xl bg-[#F3F8FF] p-4 hover:bg-[#2453CA] hover:scale-105 hover:duration-200 active:bg-green-400 rounded-xl px-3 py-2 gap-2";
+
   return (
     <div>
-
-      homeAdmin
-      <div className="flex">
-
- 
-        <Link to="/admin/employee-management" 
-        className="border rounded-xl bg-[#F3F8FF] p-4" 
-        >
-          <div className="flex flex-col items-center">
-          <MdManageAccounts className="text-4xl" />
-            <p className="text-center">Employee<br />Management</p>
-          </div>
-        </Link>
- 
-        <Link to="/admin/calendar-management"
-        className="border rounded-xl bg-[#F3F8FF] p-4"
-        >
-          <div className="flex flex-col items-center">
-          <LuCalendarClock className="text-4xl" />
-            <p className="text-center">Calendar<br />Management</p>
-          </div>
+      <p className="text-4xl font-bold p-4">Admin Management</p>
+      <div className="flex px-4 gap-4">
+        <Link to="/admin/employee-management" className={classLink}>
+          <MdManageAccounts className="text-8xl" />
+          <p className="text-center">
+            Employee
+            <br />
+            Management
+          </p>
         </Link>
 
+        <Link to="/admin/calendar-management" className={classLink}>
+          <LuCalendarClock className="text-8xl" />
+          <p className="text-center">
+            Calendar
+            <br />
+            Management
+          </p>
+        </Link>
+
+        <Link to="/admin/request-management" className={classLink}>
+          <VscGitPullRequestGoToChanges className="text-8xl" />
+          <p className="text-center">
+            Request
+            <br />
+            Management
+          </p>
+        </Link>
+
+        <Link to="/admin/payroll" className={classLink}>
+          <TbReportMoney className="text-8xl" />
+          <p className="text-center">Payroll</p>
+        </Link>
       </div>
-      
-      <div className="flex">
-
-        <Link to="/admin/request-management"
-        className="border rounded-xl bg-[#F3F8FF] p-4"
-        >
-          <div className="flex flex-col items-center">
-          <VscGitPullRequestGoToChanges className="text-4xl" />
-            <p className="text-center">Request<br />Management</p>
-          </div>
-        </Link>
-
-        <Link to="/admin/payroll"
-        className="border rounded-xl bg-[#F3F8FF] p-4"
-        >
-          <div className="flex flex-col items-center">
-          <TbReportMoney className="text-4xl" />
-            <p className="text-center">Payroll</p>
-          </div>
-        </Link>
-
-      </div>
-
     </div>
   );
 }
