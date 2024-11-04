1cfcd0079c3935adca0f9d51dfc82ff599e74692
Fullagap <Wichasith.Chaisrithong@gmail.com>
2024-11-04 15:03:24 +0700

    request form sup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import AppRoute from './routes/AppRoute'
-import AppRoute from './routes/AppRoute'
 import { ToastContainer,toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
