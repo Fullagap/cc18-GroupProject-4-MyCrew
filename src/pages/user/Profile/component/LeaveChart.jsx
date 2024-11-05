28a182a9f14e9a9542e43a0563592e944d38c8d9
Oatake <thitiwut.ekwisit@gmail.com>
2024-11-05 21:54:34 +0700

    revise leave icon

--- src/pages/user/Profile/component/LeaveChart.jsx
+++ src/pages/user/Profile/component/LeaveChart.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function LeaveChart() {
   
   return (
-    <div className="flex flex-col items-center border-4 w-full h-full">
+    <div className="flex flex-col items-center w-full h-full">
       <h1 className="text-3xl underline">Leave Allowance</h1>
       <BarChart />
     </div>
