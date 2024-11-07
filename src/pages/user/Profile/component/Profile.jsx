7c340d0962727d10eada4a68ec874fdc9376d44f
Hifaris <nattapongbe@gmail.com>
2024-11-07 10:47:18 +0700

    update: profile image when hover

--- src/pages/user/Profile/component/Profile.jsx
+++ src/pages/user/Profile/component/Profile.jsx
@@ -1,4 +1,5 @@
-import { Avatar } from "@mui/material";
+import { Avatar, CircularProgress, Box, IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
 import React, { useState, useRef } from "react";
 import { updateProfileImage } from "../../../../api/admin";
 import useAuthStore from "../../../../store/authSrore";
@@ -6,10 +7,11 @@ import useAuthStore from "../../../../store/authSrore";
 export default function Profile({ userInfo, getUserInfo }) {
   console.log(userInfo);
 
-  const token = useAuthStore((state) => state.token);
-  const user = useAuthStore((state) => state.user);
-  const [image, setImage] = useState({ id: user.id, photo: "" });
-  const formData = new FormData();
+  const token = useAuthStore((state) => state.token)
+  const user = useAuthStore((state) => state.user)
+  const [image, setImage] = useState({ id: user.id, photo: "" })
+  const [isLoading, setIsLoading] = useState(false)
+  const formData = new FormData()
 
   // Create a reference for the file input
   const fileInputRef = useRef(null);
@@ -19,15 +21,18 @@ export default function Profile({ userInfo, getUserInfo }) {
     formData.append("file", file);
     formData.append("id", user.id);
 
+    setIsLoading(true);
+
     try {
       const resp = await updateProfileImage(formData, token);
-      console.log(resp);
-      getUserInfo();
+      console.log(resp)
+      getUserInfo()
     } catch (err) {
-      console.log(err);
+      console.log(err)
+    } finally {
+      setIsLoading(false)
     }
   };
-
 
   const handleAvatarClick = () => {
     fileInputRef.current.click();
@@ -35,18 +40,64 @@ export default function Profile({ userInfo, getUserInfo }) {
 
   return (
     <div className="w-1/3 rounded-3xl bg-[#F3F8FF]">
-      <div className="justify-center flex mt-4">
-        <Avatar
-          alt="Profile"
-          src={
-            userInfo.profileImg ??
-            "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png"
-          }
-          sx={{ width: 220, height: 220, cursor: "pointer" }}
-          onClick={handleAvatarClick} 
-        />
+      <div className="justify-center flex mt-4 relative">
+     
+        <Box
+          sx={{
+            position: "relative",
+            width: 220,
+            height: 220,
+            "&:hover .editIcon": { opacity: 1 }, // Show icon on hover
+          }}
+          onClick={handleAvatarClick}
+        >
+          <Avatar
+            alt="Profile"
+            src={
+              userInfo.profileImg ??
+              "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png"
+            }
+            sx={{
+              width: 220,
+              height: 220,
+              cursor: "pointer",
+              opacity: isLoading ? 0.5 : 1,
+            }}
+          />
+
+          {isLoading && (
+            <CircularProgress
+              size={50}
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                color: "primary.main",
+              }}
+            />
+          )}
+
+       
+          <IconButton
+            className="editIcon"
+            onClick={handleAvatarClick}
+            sx={{
+              position: "absolute",
+              top: 8,
+              right: 8,
+              bgcolor: "rgba(0, 0, 0, 0.6)",
+              color: "white",
+              opacity: 0,
+              transition: "opacity 0.3s ease",
+              "&:hover": { bgcolor: "rgba(0, 0, 0, 0.8)" },
+            }}
+          >
+            <EditIcon />
+          </IconButton>
+        </Box>
       </div>
-      
+
       {/* Hidden file input that will be triggered by clicking the Avatar */}
       <input
         type="file"
