6418426e72b6974b6cfff56cc71b37ccc7bfa6d4
Hifaris <nattapongbe@gmail.com>
2024-11-14 13:58:55 +0700

    update: responsive sidebar

--- src/layout/user-layout.jsx
+++ src/layout/user-layout.jsx
@@ -5,26 +5,32 @@ import MainNav from "../components/user/MainNav";
 import useAuthStore from "../store/authSrore";
 
 const UserLayout = () => {
+  const user = useAuthStore((state) => state.user);
 
-  const user = useAuthStore((state)=>state.user)
   return (
-    <div
-      className="flex bg-neutral-100 h-screen
-      w-screen overflow-hidden"
-    > 
-    {user &&
-      <UserSidebar/>
-    }
-      <div className="flex flex-col flex-1">
-
-        {user &&
-
-        <MainNav/>
-        }
-        <div className="flex-1 p-2 min-h-0 overflow-auto bg-[#E5EDF9]">
+    <div className="relative flex bg-neutral-100 h-screen w-screen">
+      {/* Sidebar for desktop */}
+      {user && (
+        <div className="hidden md:block">
+          <UserSidebar />
+        </div>
+      )}
+      
+      <div className="flex flex-col flex-1 h-full">
+        {user && <MainNav />}
+        
+        {/* Outlet container with responsive scrolling */}
+        <div className="flex-1 p-2 overflow-y-auto bg-[#E5EDF9]">
           <Outlet />
         </div>
       </div>
+      
+      {/* Sidebar for mobile */}
+      {user && (
+        <div className="md:hidden fixed bottom-0 left-0 right-0 z-50">
+          <UserSidebar />
+        </div>
+      )}
     </div>
   );
 };
