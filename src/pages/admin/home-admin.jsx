6a4f9b8ae0eb267f521939d2509ce779befb4fbf
JP-meowmeow <jp.jirapat.t@gmail.com>
2024-11-14 15:56:52 +0700

    change button color to same color (dark blue)

--- src/pages/admin/home-admin.jsx
+++ src/pages/admin/home-admin.jsx
@@ -9,10 +9,11 @@ import { motion } from "framer-motion";  // Import motion from Framer Motion
 
 export default function HomeAdmin() {
   const classLink =
-    "group flex flex-col w-[250px] h-[150px] border w-32 rounded-xl bg-[#F3F8FF] p-4 hover:bg-[#082777] hover:text-white hover:scale-105 hover:duration-200 active:bg-green-400 rounded-xl px-3 py-2 gap-2";
-
+    // "group flex flex-col w-[250px] h-[150px] border w-32 rounded-xl bg-[#F3F8FF] p-4 hover:bg-[#082777] hover:text-white hover:scale-105 hover:duration-200 active:bg-green-400 rounded-xl px-3 py-2 gap-2";
+    "group text-white flex flex-col w-[250px] h-[150px] border w-32 rounded-xl bg-[#082777] hover:bg-blue-700 p-4 hover:text-white hover:scale-105 hover:duration-200 active:bg-green-400 rounded-xl px-3 py-2 gap-2";
+  
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="h-full bg-gray-100 p-8">
       <p className="text-4xl font-bold mb-8 text-gray-800">Admin Management</p>
       <div className="flex flex-wrap gap-20 p-10">
         <motion.div
@@ -22,7 +23,7 @@ export default function HomeAdmin() {
           transition={{ type: "spring", stiffness: 300 }}
         >
           <Link to="/admin/employee-management" className="flex flex-col w-full h-full">
-            <MdManageAccounts className="text-6xl text-blue-600 group-hover:text-white" />
+            <MdManageAccounts className="text-6xl text-white  group-hover:text-white" />
             <p className="text-center text-lg font-semibold">Employee Management</p>
           </Link>
         </motion.div>
@@ -34,7 +35,7 @@ export default function HomeAdmin() {
           transition={{ type: "spring", stiffness: 300 }}
         >
           <Link to="/admin/calendar-management" className="flex flex-col w-full h-full">
-            <LuCalendarClock className="text-6xl text-blue-600 group-hover:text-white" />
+            <LuCalendarClock className="text-6xl  text-white group-hover:text-white" />
             <p className="text-center text-lg font-semibold">Calendar Management</p>
           </Link>
         </motion.div>
@@ -46,7 +47,7 @@ export default function HomeAdmin() {
           transition={{ type: "spring", stiffness: 300 }}
         >
           <Link to="/admin/manageItem" className="flex flex-col w-full h-full">
-            <VscGitPullRequestGoToChanges className="text-6xl text-blue-600 group-hover:text-white" />
+            <VscGitPullRequestGoToChanges className="text-6xl text-white  group-hover:text-white" />
             <p className="text-center text-lg font-semibold">Item Management</p>
           </Link>
         </motion.div>
@@ -58,7 +59,7 @@ export default function HomeAdmin() {
           transition={{ type: "spring", stiffness: 300 }}
         >
           <Link to="/admin/payroll" className="flex flex-col w-full h-full">
-            <TbReportMoney className="text-6xl text-blue-600 group-hover:text-white" />
+            <TbReportMoney className="text-6xl text-white  group-hover:text-white" />
             <p className="text-center text-lg font-semibold">Payroll</p>
           </Link>
         </motion.div>
@@ -70,7 +71,7 @@ export default function HomeAdmin() {
           transition={{ type: "spring", stiffness: 300 }}
         >
           <Link to="/admin/attendance-management" className="flex flex-col w-full h-full">
-            <FaMapLocationDot className="text-6xl text-blue-600 group-hover:text-white" />
+            <FaMapLocationDot className="text-6xl text-white  group-hover:text-white" />
             <p className="text-center text-lg font-semibold">
               Attendance
               <br />
